543cd45717c04d8eba25f1965966c67c70046a8e
SamadDev <samad.00293265@gmail.com>
2026-09-22 10:24:33 +0300

    Fix: syntax error in User.php fillable array

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -22,7 +22,7 @@ class User extends Authenticatable
 
     protected $fillable = [
         'name', 'email', 'password', 'role', 'is_active',
-    】;
+    ];
 
     protected $hidden = [
         'password', 'remember_token',
